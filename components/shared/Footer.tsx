9b810d78ab2b458f62bbe9fd5d763807a720a364
werhani wassim <werhaniwassim@kelibia.r-iset.tn>
2023-12-31 20:12:34 +0100

    first commit

---
+++ components/shared/Footer.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="border-t">
+      <div className="flex-center wrapper flex-between flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href="/">
+          <Image
+            src="/assets/images/logo.svg"
+            width={128}
+            height={38}
+            alt="logo"
+          />
+        </Link>
+        <p>2023 Billet. All Rights reserved</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
