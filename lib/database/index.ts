5389cbd02220d0394e6d864464e6ef316336defd
werhani wassim <werhaniwassim@kelibia.r-iset.tn>
2024-01-02 15:43:08 +0100

    last db update

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -6,6 +6,7 @@ let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
   if (cached.conn) return cached.conn;
+
   if (!MONGODB_URI) throw new Error("MONGODB_URI is missing");
 
   cached.promise =
@@ -14,6 +15,8 @@ export const connectToDatabase = async () => {
       dbName: "evently",
       bufferCommands: false,
     });
+
   cached.conn = await cached.promise;
+
   return cached.conn;
 };
